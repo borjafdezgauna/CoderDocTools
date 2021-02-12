52914b7d3101f3ac9ced4e0287bf6d441dd5afc9
Borja Fernández Gauna <borja.fernandez@ehu.eus>
2021-02-12 14:05:13 +0100

    Updated version to 1.4

--- MarkdownToPDF/Properties/AssemblyInfo.cs
+++ MarkdownToPDF/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitHubWikiToPDF")]
+[assembly: AssemblyTitle("MarkdownToPdf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GitHubWikiToPDF")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("MarkdownToPdf")]
+[assembly: AssemblyCopyright("Copyright ©  Borja Fernandez-Gauna 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
